706123e71dc58377db6fcb013283471fb09397a2
Sualeh Fatehi <sualeh@hotmail.com>
2017-05-10 20:32:06 -0400

    Initial commit

---
+++ percolation/src/main/java/Percolation.java
@@ -0,0 +1,67 @@
+public class Percolation
+{
+  /**
+   * create n-by-n grid, with all sites blocked
+   * 
+   * @param n
+   */
+  public Percolation(int n)
+  {
+
+  }
+
+  /**
+   * open site (row, col) if it is not open already
+   * 
+   * @param row
+   * @param col
+   */
+  public void open(int row, int col)
+  {
+  }
+
+  /**
+   * is site (row, col) open?
+   * 
+   * @param row
+   * @param col
+   * @return
+   */
+  public boolean isOpen(int row, int col)
+  {
+    return false;
+  }
+
+  /**
+   * is site (row, col) full?
+   * 
+   * @param row
+   * @param col
+   * @return
+   */
+  public boolean isFull(int row, int col)
+  {
+    return false;
+  }
+
+  /**
+   * number of open sites
+   * 
+   * @return
+   */
+  public int numberOfOpenSites()
+  {
+    return 0;
+  }
+
+  /**
+   * does the system percolate?
+   * 
+   * @return
+   */
+  public boolean percolates()
+  {
+    return false;
+  }
+
+}
